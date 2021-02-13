ebad750fd64234fd122237ea3a36392b82d88a76
Angel Sinn <angelkysinn@gmail.com>
2021-02-13 00:16:57 +0000

    LightCoin assignment

--- index.js
+++ index.js
@@ -1,29 +1,85 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date().toString();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
 
+  isAllowed() {
+    //note how it has access to this.account b/c of parent
+    return this.account.balance - this.amount >= 0;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 
+  isAllowed() {
+    return true;
+  }
+}
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Starting Account Balance: ", myAccount.balance);
+console.log("*************************");
 
-console.log('Balance:', balance);
+const t1 = new Withdrawal(10, myAccount);
+console.log("Commit result:", t1.commit());
+console.log("Account Balance: ", myAccount.balance);
+console.log("*************************");
+
+const t2 = new Deposit(100, myAccount);
+console.log("Commit result:", t2.commit());
+console.log("Account Balance: ", myAccount.balance);
+console.log("*************************");
+
+const t3 = new Withdrawal(50, myAccount);
+console.log("Commit result:", t3.commit());
+console.log("Account Balance: ", myAccount.balance);
+console.log("*************************");
+
+const t4 = new Deposit(25, myAccount);
+console.log("Commit result:", t4.commit());
+console.log("Account Balance: ", myAccount.balance);
+console.log("*************************");
+
+console.log("Ending Account Balance: ", myAccount.balance);
+
+console.log("------------------------------------");
+console.log("Account Transaction History: ", myAccount.transactions);
